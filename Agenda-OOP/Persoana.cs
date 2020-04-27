d9469d49e7fb420fd8fe422be65bb2479d3942c4
Rafael <rafaelbuksa@gmail.com>
2020-04-27 19:59:37 +0300

    Created class Persoana

---
+++ Agenda-OOP/Persoana.cs
@@ -0,0 +1,19 @@
+﻿namespace Agenda_OOP
+{
+    internal class Persoana
+    {
+        public string nume { get; set; }
+
+
+
+        public Persoana()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return nume;
+        }
+    }
+}
--- Agenda-OOP/Program.cs
+++ Agenda-OOP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,41 @@ namespace Agenda_OOP
     {
         static void Main(string[] args)
         {
+            //Asta este cea mai buna tema pe care am primit-o, serios :D
+            List<Persoana> persoane = new List<Persoana>();
+            string nume;
+        Start:
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+            Console.WriteLine("Legend:");
+            Console.WriteLine("C - Create new person");
+            if (persoane.Count() > 0)
+            {
+                Console.WriteLine($"1-{persoane.Count()} - Select person");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                foreach (Persoana pers in persoane)
+                {
+                    Console.WriteLine(pers);
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
 
+            //AutoRead();
+            nume = Console.ReadLine();
+            persoane.Add(new Persoana() { nume = nume });
+            goto Start;
+        }
 
-
-
-
+        private static void AutoRead()
+        {
+            StreamReader sr = new StreamReader("..\\..\\Persoane.txt");
+            using (sr)
+            {
+                while (sr.Peek() > 0)
+                {
+                    string nume = sr.ReadLine();
+                }
+            }
         }
     }
 }
